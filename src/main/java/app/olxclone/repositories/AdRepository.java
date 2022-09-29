4ac2368a5d47df9afb1239357f9c0404cfe6f7b7
stefanoprea02 <stefanoprea2002@gmail.com>
2022-09-29 15:12:40 +0300

    Fixed image upload.

--- src/main/java/app/olxclone/repositories/AdRepository.java
+++
@@ -1,8 +0,0 @@
-package app.olxclone.repositories;
-
-import app.olxclone.domain.Ad;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface AdRepository extends ReactiveMongoRepository<Ad, String> {
-}
--- src/main/java/app/olxclone/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package app.olxclone.repositories;
-
-import app.olxclone.domain.User;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface UserRepository extends ReactiveMongoRepository<User, String> {
-}
